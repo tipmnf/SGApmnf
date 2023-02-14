11a17d59986454ed6a672dc27cd0cb23dcf053a3
YanIgnacio <yandasilva3@gmail.com>
2023-02-14 11:30:48 -0300

    2.0.2 teste3

--- atendimento/views.py
+++ atendimento/views.py
@@ -4,7 +4,6 @@ from .models import Atendimento, TipoAtendimento, Atendente
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from escpos.printer import Usb
-from jinja2 import Template
 # Create your views here.
 
 @login_required
@@ -209,15 +208,9 @@ def finalizarSemAtendimento(request):
 def imprimeSenha(request, atendimento):
 
     printer = Usb(0x4b8, 0xe03)
+    senha = f"{atendimento.tipo_atendimento.prefixo}{atendimento.numero_senha}"
 
-    template_str = """
-        <span style="font-size: 24pt">{{ atendimento.tipo_atendimento.prefixo }}{{ atendimento.numero_senha }}</span>
-    """
-
-    template = Template(template_str)
-    output = template.render(atendimento=atendimento)
-
-    printer.text(output + "\n")
+    printer.text("{\\H1}" + senha + "{\\H1}")
     printer.cut()
 
     printer.close()
