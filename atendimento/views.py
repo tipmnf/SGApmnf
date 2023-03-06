3506ec1caec1decfc5389974ccde3802c062c788
YanIgnacio <yandasilva3@gmail.com>
2023-03-06 14:39:29 -0300

    5.0.1 fix and tests

--- atendimento/views.py
+++ atendimento/views.py
@@ -19,10 +19,10 @@ def gerar_senha(request):
             atendimento.save()
             form = GerarSenhaForm()
             context={'form': form, 'tipos_atendimento': TipoAtendimento.objects.all(), 'atendimento': atendimento}
-            try:
-                imprimeSenha(request, atendimento)
-            except:
-                return render(request, 'erro.html', context)
+            # try:
+            imprimeSenha(request, atendimento)
+            # except:
+            #     return render(request, 'erro.html', context)
             return render(request, 'gerar_senha.html', context)        
     context={'form': form, 'tipos_atendimento': TipoAtendimento.objects.all()}
 
@@ -234,7 +234,7 @@ from senhaFacil.settings import BASE_DIR, PROJECT_ROOT
 @login_required
 def imprimeSenha(request, atendimento):
 
-    printer = Serial(devfile='/dev/ttyS0', baudrate='115200', bytesize='8', parity='N', stopbits=1)
+    printer = Serial(devfile='/dev/ttyS1', baudrate='115200', bytesize='8', parity='N', stopbits=1)
     senha = atendimento.tipo_atendimento.prefixo + str(atendimento.numero_senha).zfill(3)
     data = date.today()
     dataStr = data.strftime("Data: %d/%m/%Y\n")
