27ea337f547206c72b2c331c275dd92659506d4b
YanIgnacio <yandasilva3@gmail.com>
2023-03-15 15:36:24 -0300

    5.6.6 test

--- atendimento/views.py
+++ atendimento/views.py
@@ -250,15 +250,15 @@ def imprimeSenha(request, atendimento):
         job = win32print.StartDocPrinter(printer, 1, ('Test print', None, "RAW"))
         try:
             win32print.WritePrinter(printer, "\x1B!1".encode('utf-8'))
-            win32print.WritePrinter(printer, "\x1Ba1".encode('utf-8'))
+            win32print.WritePrinter(printer, "\x1Ba".encode('utf-8'))
             win32print.WritePrinter(printer, "\b SENHA:\n\n".encode('utf-8'))
-            win32print.WritePrinter(printer, "\x1B!20".encode('utf-8')) 
+            win32print.WritePrinter(printer, "\x1B!9".encode('utf-8')) 
             win32print.WritePrinter(printer, "\x1BE".encode('utf-8')) # bold font
             win32print.WritePrinter(printer, f"{senha}\n\n".encode('utf-8'))
             win32print.WritePrinter(printer, "\x1B!1".encode('utf-8'))
             win32print.WritePrinter(printer, "\x1BF".encode('utf-8')) # normal font
             win32print.WritePrinter(printer, "Prefeitura Municipal de Nova Friburgo\n".encode('utf-8'))
-            win32print.WritePrinter(printer, f"{dataStr}\n\n\n\n\n".encode('utf-8'))
+            win32print.WritePrinter(printer, f"{dataStr}\n\n".encode('utf-8'))
             win32print.WritePrinter(printer, "\x1Bm".encode('utf-8'))
         finally:
             win32print.EndPagePrinter(printer)
