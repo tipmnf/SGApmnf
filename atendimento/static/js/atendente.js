52a3091eccca1799fa575e1b8814445e2352b07e
YanIgnacio <yandasilva3@gmail.com>
2023-03-20 15:46:44 -0300

    6.0.5 contagem da fila e ajuste display

--- atendimento/static/js/atendente.js
+++ atendimento/static/js/atendente.js
@@ -33,7 +33,54 @@ function busca(){
         });
     }
 
-// busca()
-// setInterval(function() {
-//     busca()
-// }, 5000);
+// conta as filas para mostrar ao atendente
+function contaFila(dados) {    
+    var pessoasFila = document.getElementById("quantFila");
+    pessoasFila.innerHTML = '0';
+    var pessoasPref = document.getElementById("quantPref");
+    pessoasPref.innerHTML = '0';
+    var pessoasProc = document.getElementById("quantProc");
+    pessoasProc.innerHTML = '0';
+
+    console.log("estou contando");
+
+    var numPessoas = 0;
+    var numPessoasPref = 0;
+    var numPessoasProc = 0
+
+    dados.forEach(function(dado) {
+        if(dado.status == 'fila'){
+            if(dado.tipo == 'Geral'){
+                numPessoas = numPessoas + 1;
+            }
+            if(dado.tipo == 'Preferencial'){
+                numPessoasPref = numPessoasPref + 1;
+            }
+            if(dado.tipo == 'Processos'){
+                numPessoasProc = numPessoasProc + 1;
+            }
+        }
+    });    
+    
+    console.log("contei:", numPessoas, numPessoasPref, numPessoasProc);
+
+    pessoasFila.innerHTML = numPessoas;
+    pessoasPref.innerHTML = numPessoasPref;
+    pessoasProc.innerHTML = numPessoasProc;
+
+}
+
+function getFilas(){
+    fetch("/tabela-dados-fila/")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(dados) {
+            contaFila(dados);
+        });
+}
+
+busca()
+setInterval(function() {
+    getFilas()
+}, 5000);
