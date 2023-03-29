a62160838866e8d90446de7150c6b17e35cc49a4
YanIgnacio <yandasilva3@gmail.com>
2023-03-29 10:45:20 -0300

    6.3.3 inserção de segundo banco

--- senhaFacil/settings.py
+++ senhaFacil/settings.py
@@ -61,6 +61,11 @@ WSGI_APPLICATION = 'senhaFacil.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'remote_db': {
         'ENGINE': 'django.db.backends.mysql',
 
         'NAME': 'dev_senhas_facil',
