30fcf752ae995e8595cf68147fe91a9b1faccda9
YanIgnacio <yandasilva3@gmail.com>
2023-04-25 14:00:29 -0300

    esqueci de mudar pro banco remoto xd

--- senhaFacil/settings.py
+++ senhaFacil/settings.py
@@ -60,21 +60,21 @@ WSGI_APPLICATION = 'senhaFacil.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-
-    #     'NAME': 'senhas_facil_b',
-    #     'PORT': '3306',
-
-    #     'USER': 'yan',
-    #     'PASSWORD': '1617021115fam9@@',
-    #     'HOST': '192.168.1.159',
-    # },
-    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'senhas_facil_b',
+        'PORT': '3306',
+
+        'USER': 'yan',
+        'PASSWORD': '1617021115fam9@@',
+        'HOST': '192.168.1.159',
+    },
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
