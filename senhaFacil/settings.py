c18dacbb397d4b4bae622f3fab87049bc3070d09
gustavo <gustavonerybarbosa@gmail.com>
2023-12-01 11:27:49 -0300

    remote db

--- senhaFacil/settings.py
+++ senhaFacil/settings.py
@@ -61,21 +61,21 @@ WSGI_APPLICATION = 'senhaFacil.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-
-    #     'NAME': 'senhas_facil_b',
-    #     'PORT': '3306',
-
-    #     'USER': 'senhas_facil_b',
-    #     'PASSWORD': 'senhas_facil_123654*mudar',
-    #     'HOST': '192.168.1.220',
-    # },
-    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'senhas_facil_b',
+        'PORT': '3306',
+
+        'USER': 'senhas_facil_b',
+        'PASSWORD': 'senhas_facil_123654*mudar',
+        'HOST': '192.168.1.220',
+    },
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
